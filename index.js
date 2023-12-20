2f68e2e0802ecbc86d382420f9da324d8c912848
aragabz <ahmedragab.se@gmail.com>
2023-12-20 02:45:13 +0200

    remove default design and add redux toolkit

--- index.js
+++ index.js
@@ -5,7 +5,9 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import {store} from "./src/redux/store/store";
+import {store} from "./src/redux/store";
+import {Provider} from "react-redux";
+
 
 
 const ReduxProvider = () => {
