e89c78658faa2494f5627f40456e859d3dfd9acd
aragabz <ahmedragab.se@gmail.com>
2023-12-20 02:19:38 +0200

    create app structure with fixing android build and change index.js to add redux provider

--- index.js
+++ index.js
@@ -5,5 +5,13 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {store} from "./src/redux/store/store";
 
-AppRegistry.registerComponent(appName, () => App);
+
+const ReduxProvider = () => {
+    return <Provider store={store}>
+        <App />
+    </Provider>
+}
+
+AppRegistry.registerComponent(appName, () => ReduxProvider);
